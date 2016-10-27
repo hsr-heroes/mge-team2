b2d2f604d0f865b9e69473e964346afce6af1f19
Dominik Bruderer <dominikbruderer@gmail.com>
2016-10-27 13:17:55 +0200

    Add custom server field for login

--- Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/LoginActivity.java
+++ Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/LoginActivity.java
@@ -3,10 +3,12 @@ package ch.hsr_heroes.gadgeothek;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
@@ -14,8 +16,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    private TextInputEditText inputEmail, inputPassword;
-    private TextInputLayout inputLayoutEmail, inputLayoutPassword;
+    private TextInputEditText inputEmail, inputPassword, inputCustomURI;
+    private TextInputLayout inputLayoutEmail, inputLayoutPassword, inputLayoutCustomServerURI;
+    private SwitchCompat switchCustomServer;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +28,19 @@ public class LoginActivity extends AppCompatActivity {
 
         this.inputEmail = (TextInputEditText) findViewById(R.id.inputEmail);
         this.inputPassword = (TextInputEditText) findViewById(R.id.inputPassword);
+        this.inputCustomURI = (TextInputEditText) findViewById(R.id.input_custom_server_uri);
 
         this.inputLayoutEmail = (TextInputLayout) findViewById(R.id.inputLayoutEmail);
         this.inputLayoutPassword = (TextInputLayout) findViewById(R.id.inputLayoutPassword);
+        this.inputLayoutCustomServerURI = (TextInputLayout) findViewById(R.id.input_layout_custom_server_uri);
+
+        this.switchCustomServer = (SwitchCompat) findViewById(R.id.switch_custom_server);
+
+        this.buttonLogin = (Button) findViewById(R.id.login);
 
         inputEmail.addTextChangedListener(new LocalTextWatcher(inputEmail));
         inputPassword.addTextChangedListener(new LocalTextWatcher(inputPassword));
+        inputCustomURI.addTextChangedListener(new LocalTextWatcher(inputCustomURI));
 
         Button buttonSignUp = (Button) findViewById(R.id.create_an_account);
 
@@ -41,7 +52,20 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button buttonLogin = (Button) findViewById(R.id.login);
+
+        final SwitchCompat switchCustomServer = this.switchCustomServer;
+        final TextInputLayout customServer = this.inputLayoutCustomServerURI;
+
+        switchCustomServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (switchCustomServer.isChecked()) {
+                    customServer.setVisibility(View.VISIBLE);
+                } else {
+                    customServer.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +78,19 @@ public class LoginActivity extends AppCompatActivity {
         SharedPreferences preferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
         String authToken = preferences.getString("auth_token", null);
+    }
+
+    private void validateURI() {
+        String uriString = inputCustomURI.getText().toString().trim();
+
+        Uri uri = Uri.parse(uriString);
+
+        if (uri.toString().length() < 5) {
+            inputLayoutCustomServerURI.setError("Please enter a valid URI");
+        } else {
+            inputLayoutCustomServerURI.setErrorEnabled(false);
+        }
+
     }
 
     private void validateEmail() {
@@ -107,6 +144,19 @@ public class LoginActivity extends AppCompatActivity {
                 case R.id.inputPassword:
                     validatePassword();
                     break;
+                case R.id.input_custom_server_uri:
+                    validateURI();
+                    break;
+            }
+
+            // Disable submit button if an error is present
+            // https://material.google.com/patterns/errors.html#errors-user-input-errors
+            if (LoginActivity.this.inputLayoutEmail.isErrorEnabled() ||
+                    LoginActivity.this.inputLayoutPassword.isErrorEnabled() ||
+                    (LoginActivity.this.switchCustomServer.isChecked() && LoginActivity.this.inputLayoutCustomServerURI.isErrorEnabled())) {
+                LoginActivity.this.buttonLogin.setEnabled(false);
+            } else {
+                LoginActivity.this.buttonLogin.setEnabled(true);
             }
         }
     }
