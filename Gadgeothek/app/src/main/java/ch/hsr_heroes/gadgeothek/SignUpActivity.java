5e36bc565938943dda9568ce295a9f7a91808b44
Dominik Bruderer <dominikbruderer@gmail.com>
2016-10-28 13:57:22 +0200

    Add sign up activity implementation

--- Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/SignUpActivity.java
+++ Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/SignUpActivity.java
@@ -1,13 +1,163 @@
 package ch.hsr_heroes.gadgeothek;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import ch.hsr_heroes.gadgeothek.service.Callback;
+import ch.hsr_heroes.gadgeothek.service.LibraryService;
+
+public class SignUpActivity extends AppCompatActivity implements CustomServerPartialFragment.CustomServerListener {
+    private TextInputEditText inputName;
+    private TextInputEditText inputEmail;
+    private TextInputEditText inputPassword;
+    private TextInputEditText inputStudentNumber;
+    private TextInputLayout inputLayoutName;
+    private TextInputLayout inputLayoutEmail;
+    private TextInputLayout inputLayoutPassword;
+    private TextInputLayout inputLayoutStudentNumber;
+    private Button buttonSignUp;
+    private CustomServerPartialFragment customServerFragment;
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+
+        if (fragment instanceof CustomServerPartialFragment) {
+            customServerFragment = (CustomServerPartialFragment) fragment;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        customServerFragment.setDescription("Do you want to register on a custom server?");
+
+        inputName = (TextInputEditText) findViewById(R.id.input_name);
+        inputEmail = (TextInputEditText) findViewById(R.id.input_email);
+        inputPassword = (TextInputEditText) findViewById(R.id.input_password);
+        inputStudentNumber = (TextInputEditText) findViewById(R.id.input_student_number);
+
+        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        inputLayoutEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
+        inputLayoutPassword = (TextInputLayout) findViewById(R.id.input_layout_password);
+        inputLayoutStudentNumber = (TextInputLayout) findViewById(R.id.input_layout_student_number);
+
+        inputName.addTextChangedListener(new SignUpActivity.LocalTextWatcher(inputName));
+        inputEmail.addTextChangedListener(new SignUpActivity.LocalTextWatcher(inputEmail));
+        inputPassword.addTextChangedListener(new SignUpActivity.LocalTextWatcher(inputPassword));
+        inputStudentNumber.addTextChangedListener(new SignUpActivity.LocalTextWatcher(inputStudentNumber));
+
+        buttonSignUp = (Button) findViewById(R.id.button_sign_up);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = inputName.getText().toString();
+                final String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+                final String studentNumber = inputStudentNumber.getText().toString();
+
+                LibraryService.setServerAddress(customServerFragment.getServer());
+
+                LibraryService.register(email, password, name, studentNumber, new Callback<Boolean>() {
+                    @Override
+                    public void onCompletion(Boolean registered) {
+                        if (registered) {
+                            LibraryService.login(email, password, new Callback<Boolean>() {
+                                @Override
+                                public void onCompletion(Boolean loggedIn) {
+                                    if (loggedIn) {
+                                        startActivity(new Intent(SignUpActivity.this, GadgetListActivity.class));
+                                        Toast.makeText(SignUpActivity.this, R.string.login_successful, Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, R.string.login_failed, Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                                    }
+                                }
+
+                                @Override
+                                public void onError(String message) {
+                                    Toast.makeText(SignUpActivity.this, getString(R.string.no_server_connection) + "\n" + message, Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                                }
+                            });
+                        } else {
+                            Toast.makeText(SignUpActivity.this, R.string.registration_failed, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(SignUpActivity.this, getString(R.string.no_server_connection) + "\n" + message, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onServerChanged(String newServer, boolean valid) {
+        buttonSignUp.setEnabled(isValidForm());
+    }
+
+    private class LocalTextWatcher implements TextWatcher {
+
+        private View view;
+
+        public LocalTextWatcher(View view) {
+            this.view = view;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    ValidationHelper.validateName(SignUpActivity.this, inputLayoutName, inputName);
+                    break;
+                case R.id.input_email:
+                    ValidationHelper.validateEmail(SignUpActivity.this, inputLayoutEmail, inputEmail);
+                    break;
+                case R.id.input_password:
+                    ValidationHelper.validatePassword(SignUpActivity.this, inputLayoutPassword, inputPassword);
+                    break;
+                case R.id.input_student_number:
+                    ValidationHelper.validateStudentNumber(SignUpActivity.this, inputLayoutStudentNumber, inputStudentNumber);
+                    break;
+            }
+
+            // Disable submit button if an error is present or field is empty
+            // https://material.google.com/patterns/errors.html#errors-user-input-errors
+            buttonSignUp.setEnabled(isValidForm());
+        }
+    }
+
+    private boolean isValidForm() {
+        return (ValidationHelper.isValidName(inputName) &&
+                ValidationHelper.isValidEmail(inputEmail) &&
+                ValidationHelper.isValidPassword(inputPassword) &&
+                ValidationHelper.isValidStudentNumber(inputStudentNumber) &&
+                (!customServerFragment.isChecked() || (customServerFragment.isChecked() && customServerFragment.isValid())));
     }
 }
--- Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/ValidationHelper.java
+++ Gadgeothek/app/src/main/java/ch/hsr_heroes/gadgeothek/ValidationHelper.java
@@ -1,40 +1,42 @@
 package ch.hsr_heroes.gadgeothek;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.util.Patterns;
 
 import okhttp3.HttpUrl;
 
-public class ValidationHelper {
+class ValidationHelper {
 
-    public static boolean isValidEmail(String email) {
+    private static boolean isValidEmail(String email) {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    public static boolean isValidEmail(TextInputEditText editText) {
+    static boolean isValidEmail(TextInputEditText editText) {
         return isValidEmail(extractString(editText));
     }
 
-    public static boolean isValidPassword(String password) {
+    private static boolean isValidPassword(String password) {
         return !password.isEmpty();
     }
 
-    public static boolean isValidPassword(TextInputEditText editText) {
+    static boolean isValidPassword(TextInputEditText editText) {
         return isValidPassword(extractString(editText));
     }
 
-    public static boolean isValidCustomServer(String customServer) {
+    static boolean isValidCustomServer(String customServer) {
         HttpUrl uri = HttpUrl.parse(customServer);
 
         return !(uri == null);
     }
 
-    public static boolean isValidCustomServer(TextInputEditText editText) {
+    static boolean isValidCustomServer(TextInputEditText editText) {
         return isValidCustomServer(extractString(editText));
     }
 
-    public static boolean isEmpty(TextInputEditText editText) {
+    private static boolean isEmpty(TextInputEditText editText) {
         return extractString(editText).isEmpty();
     }
 
@@ -42,15 +44,51 @@ public class ValidationHelper {
         return editText.getText().toString().trim();
     }
 
-    public static boolean isValidName(TextInputEditText editText) {
+    static boolean isValidName(TextInputEditText editText) {
         return !extractString(editText).isEmpty();
     }
 
-    public static boolean isValidStudentNumber(TextInputEditText editText) {
+    static boolean isValidStudentNumber(TextInputEditText editText) {
         try {
             return 0 < Integer.valueOf(extractString(editText));
         } catch(NumberFormatException e) {
             return false;
+        }
+    }
+
+    static void validateEmail(Context context, TextInputLayout inputLayoutEmail, TextInputEditText inputEmail) {
+        if (!ValidationHelper.isEmpty(inputEmail)) {
+            if (ValidationHelper.isValidEmail(inputEmail)) {
+                inputLayoutEmail.setErrorEnabled(false);
+            } else {
+                inputLayoutEmail.setError(context.getString(R.string.enter_a_valid_email_address));
+            }
+        } else {
+            inputLayoutEmail.setError(context.getString(R.string.email_address_required));
+        }
+    }
+
+    static void validatePassword(Context context, TextInputLayout inputLayoutPassword, TextInputEditText inputPassword) {
+        if (ValidationHelper.isValidPassword(inputPassword)) {
+            inputLayoutPassword.setErrorEnabled(false);
+        } else {
+            inputLayoutPassword.setError(context.getString(R.string.password_required));
+        }
+    }
+
+    static void validateName(Context context, TextInputLayout inputLayoutName, TextInputEditText inputName) {
+        if (ValidationHelper.isValidName(inputName)) {
+            inputLayoutName.setErrorEnabled(false);
+        } else {
+            inputLayoutName.setError(context.getString(R.string.name_required));
+        }
+    }
+
+    static void validateStudentNumber(Context context, TextInputLayout inputLayoutStudentNumber, TextInputEditText inputStudentNumber) {
+        if (ValidationHelper.isValidStudentNumber(inputStudentNumber)) {
+            inputLayoutStudentNumber.setErrorEnabled(false);
+        } else {
+            inputLayoutStudentNumber.setError(context.getString(R.string.student_number_required));
         }
     }
 }
